2117f7264a95b16a19dbe43a5984c707cc735b6b
James Benjamin <james.abenj@gmail.com>
2019-10-21 22:10:33 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,9 +1,12 @@
 class Course 
   attr_accessor :title, :schedule, :description
   
+    @@all = []
+    
   def initialize
     @@all << self 
-  @@all = []
+  end 
+
   
   def self.all 
     @@all 
